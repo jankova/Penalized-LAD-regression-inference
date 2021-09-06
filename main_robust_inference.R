fad7e920eda21d7669b6446e38e643d015065b57
jankova <60160038+jankova@users.noreply.github.com>
2021-09-06 23:25:17 +0200

    Add files via upload

--- main_robust_inference.R
+++ main_robust_inference.R
@@ -1,30 +1,17 @@
-
 source('requirements.R')
 source('utils.R')
 
 n <- 200
 p <- 10
 
-diag.value <- 1
-rho <- 0.3
-lower <- rbind(rep(0,p), (diag(p))[-p,])
-upper <- t(lower)
-Theta0 <- diag.value * diag(p) + rho * (upper + lower)
-Sigma0 <- solve(Theta0)
-
-sqrSigma <- sqrtm(Sigma0)
-
-# the regression vector
-beta <- rep(0,p)
-beta[1:3] <- c(1, 1, 1)
-
 iter <- 500
-
-length <- coverage <- rep(0,p)
+length <- coverage <- rep(0, p)
 estVar <- est <- matrix(0, nrow = iter, ncol = p)
-sig <- 1
+lambdas.qr <- seq(10, 60, 2)
+pars <- pars_init(n, p)
+Sigma0 <- pars$Sigma0
+beta <- pars$beta
 
-lambdas.qr <- seq(10,60,2)
 
 pb <- txtProgressBar(0, iter, style = 3)
 
@@ -32,35 +19,26 @@ set.seed(1)
 
 for(i in 1:iter){
   setTxtProgressBar(pb, i)
-  # Generate data 
+  
+  # Generate data:
+  # Errors with t-distribution with 3 degrees of freedom
   nu <- 3
-  # errors with t-distribution with 3 degrees of freedom
   eps <- rt(n,nu) / sqrt(nu/(nu-2)) 
   X <- mvrnorm(n, rep(0,p), Sigma0)
   Y <- X %*% beta + eps
   
   ## Compute de-biased LAD estimator 
-  Theta <- nodewise(X, p, n, sqrt(log(p)/n))
-  
-  # LAD estimator
-  lam <- lambdas.qr[which.min(cv.qr(X,Y,lambdas.qr,n))]
-  
-  f <- rq.fit.lasso(X, Y, tau = 0.5,beta = .9995, eps = 1e-06)
-  beta.hat <- f$coefficients
-  lamZ <-  t(X) %*% (sign(Y-X%*% beta.hat))  /n
-
-  f0 <- dt(0,nu)*sqrt(nu/(nu-2))
-  beta.ds <- beta.hat +  1/2/ f0 *Theta  %*% (lamZ)  
-  
-  # Compute confidence intervals 
-  sigma <-  1/4/f0^2 *(diag(Theta)) # diag(Theta)
-  rad <- qnorm(0.975)*sqrt(sigma) / sqrt(n)
-  LE <- beta.ds - rad 
-  UE <- beta.ds + rad
+  out <- debiasedLAD(n, p, X, Y, lambdas.qr, nu)
+  UE = out$UE
+  LE = out$LE
+  beta.ds = out$beta.ds
+  sigma = out$sigma
+  Theta = out$Theta
+  f0 = out$f0
   
   # Compute average coverage and length
-  length <-  length + 2*rad
-  coverage <- coverage + as.numeric((beta <= UE)*(beta >= LE)==1)
+  length <-  length + 2 * (UE - LE)
+  coverage <- coverage + as.numeric((beta <= UE)*(beta >= LE) == 1)
   
   est[i,] <- beta.ds
   estVar[i,] <- sigma
@@ -70,10 +48,8 @@ for(i in 1:iter){
 pb.close()
 
 # Print results
-average_lengths <- length/iter
-average_coverages <- coverage/iter
-round(average_lengths,3) 
-round(average_coverages,3) 
+round(length/iter,3) 
+round(coverage/iter,3) 
 
 # Plot histograms
 expr <- c(expression(beta [1]),expression(beta [2]),expression(beta [3]),expression(beta [4]))
@@ -81,6 +57,6 @@ par(mar=c(1,1,1,1)*2)
 par(mfrow=c(2,2))
 
 for(j in 1:4){
-  his(n,odhady,beta,Theta/4/f0^2,expr[j],j,12,"De-biased LAD")
+  his(n, est, beta, Theta/4/f0^2, expr[j], j, 12, "De-biased LAD")
 }
 
--- utils.R
+++ utils.R
@@ -31,14 +31,57 @@ nodewise <- function(x, p, n,lambdaj){
 }
 
 ## Histogram plotting
-his <- function(n,odhady,beta,vari,expr,j,nc,labe){
+his <- function(n,est,beta,vari,expr,j,nc,labe){
   
-  hist(sqrt(n)*( ( odhady[,j]-beta[j] )/sqrt(vari[j,j]) ), 
+  hist(sqrt(n)*( ( est[,j]-beta[j] )/sqrt(vari[j,j]) ), 
        nclass=nc, 
        probability=TRUE,
        main = "", xlab=expr, ylab="Density",
        xlim=c(-3.2,3.2),ylim=c(0,0.5),col="grey",yaxt="n")
   
   lines(seq(-4,4,by=0.1), dnorm(seq(-4,4,by=0.1)),col="red")
+  
+}
+
+
+pars_init <- function(n, p){
+  
+  diag.value <- 1
+  rho <- 0.3
+  lower <- rbind(rep(0,p), (diag(p))[-p,])
+  upper <- t(lower)
+  Theta0 <- diag.value * diag(p) + rho * (upper + lower)
+  Sigma0 <- solve(Theta0)
+  
+  # the regression vector
+  beta <- rep(0, p)
+  beta[1:3] <- c(1, 1, 1)
+  
+  list(Sigma0 = Sigma0, beta = beta)
+  
+}
+
+
+debiasedLAD <- function(n, p, X, Y, lambdas.qr, nu){
+  Theta <- nodewise(X, p, n, sqrt(log(p)/n))
+  
+  # LAD estimator
+  lam <- lambdas.qr[which.min(cv.qr(X,Y,lambdas.qr,n))]
+  
+  f <- rq.fit.lasso(X, Y, tau = 0.5, beta = .9995, eps = 1e-06)
+  beta.hat <- f$coefficients
+  lamZ <-  t(X) %*% sign(Y - X %*% beta.hat)  /n
+  
+  f0 <- dt(0, nu) * sqrt(nu / (nu-2))
+  beta.ds <- beta.hat +  1/2/ f0 * Theta %*% (lamZ)  
+  
+  # Compute confidence intervals 
+  sigma <-  1/4/f0^2 * diag(Theta)
+  rad <- qnorm(0.975) * sqrt(sigma) / sqrt(n)
+  LE <- beta.ds - rad 
+  UE <- beta.ds + rad
+  
+  list(LE = LE, UE = UE, sigma = sigma, 
+       beta.ds = beta.ds, Theta = Theta, f0 = f0)
   
 }
